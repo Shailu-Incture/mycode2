4b0f3834138be2d3a81d15d2e8dfa24ddd7a66b8
Shailu-Incture <Shailesh.Shetty@Incture.com>
2018-10-17 15:02:39 +0530

    Initial Push

---
+++ webapp/controller/View1.controller.js
@@ -0,0 +1,9 @@
+sap.ui.define([
+	"sap/ui/core/mvc/Controller"
+], function (Controller) {
+	"use strict";
+
+	return Controller.extend("sampleApplication2.sampleApplication2.controller.View1", {
+
+	});
+});
